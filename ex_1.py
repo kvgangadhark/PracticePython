479c4a8a57bbb02905a9f204c60f91a1c042143b
kvgangadhark <kvgangadhark@gmail.com>
2016-01-29 13:13:09 +0530

    adding the code other parts of the question in exercise 1.

--- ex_1.py
+++ ex_1.py
@@ -1,3 +1,6 @@
 name = input("give your name:")
+age = int(input("give your age:"))
 print ("hi" + name+"!")
-
+tgt= 2016+100-age
+print (tgt)
+print ("you will be a hundred years old by year"+str(tgt))
