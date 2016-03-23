145772a4d10a8de7c8e694dec7d619c34c6a1050
kvgangadhark <kvgangadhark@gmail.com>
2016-03-23 16:07:01 +0530

    added logic to deduct individual field names from the structure definition.

--- src_gen_hdr.py
+++ src_gen_hdr.py
@@ -2,3 +2,17 @@
 # and generates c source code that will print each member when mapped
 # with binary data.
 
+fields=[]
+f1 = open("nic_desc.txt","r")
+for eachline in f1:
+    lst = eachline.split(" ")
+    for each in lst:
+        if each == ' ' or each == '' or each == 'u32' or each == "UEXACT16" or each == "UEXACT32":
+            lst.remove(each)
+    #print (lst)
+    fields.append(lst[0])
+
+for each in fields:
+    print (each)
+    
+
